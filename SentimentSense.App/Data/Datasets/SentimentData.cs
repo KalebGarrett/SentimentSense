e913f1c051072e65196b703e2293727a650182f0
Kaleb Garrett <kalebagarrett@gmail.com>
2024-07-25 15:44:30 -0400

    Added sentiment data

---
+++ SentimentSense.App/Data/Datasets/SentimentData.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.ML.Data;
+
+namespace SentimentSense.App.Data.Datasets;
+
+public class SentimentData
+{
+    [LoadColumn(0)] public string SentimentText { get; set; }
+    [LoadColumn(1)] public bool Sentiment { get; set; }
+}
