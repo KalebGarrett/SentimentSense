98e377f3191166663504448545b402e0d89a1866
Kaleb Garrett <kalebagarrett@gmail.com>
2024-07-25 16:24:27 -0400

    Added interface for machine learning algorithms

---
+++ SentimentSense.App/Services/Interfaces/IMachineLearningService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML;
+using static Microsoft.ML.DataOperationsCatalog;
+
+namespace SentimentSense.App.Services.Interfaces;
+
+public interface IMachineLearningService
+{
+    TrainTestData LoadData(MLContext ctx);
+    ITransformer BuildAndTrainModel(MLContext mlContext, IDataView splitTrainSet);
+}
