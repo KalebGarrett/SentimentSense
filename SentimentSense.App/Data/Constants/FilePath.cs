97a7616e6d789b529636ab50c72bbeb4149bd75d
Kaleb Garrett <kalebagarrett@gmail.com>
2024-07-25 16:10:15 -0400

    Added collection for file paths

---
+++ SentimentSense.App/Data/Constants/FilePath.cs
@@ -0,0 +1,7 @@
+﻿namespace SentimentSense.App.Data.Constants;
+
+public class FilePath
+{
+    public const string SentimentDataCsv 
+        = @"D:\repos\SentimentSense\SentimentSense.App\Data\Docs\sentiment_data.csv";
+}
