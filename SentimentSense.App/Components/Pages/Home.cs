81a97f0f02f50fb28cd1760cadddc586b394e5d6
Kaleb Garrett <kalebagarrett@gmail.com>
2024-07-25 17:07:12 -0400

    Added form to test binary classification model

---
+++ SentimentSense.App/Components/Pages/Home.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.ML;
+using SentimentSense.App.Data.Datasets;
+using SentimentSense.App.Services;
+
+namespace SentimentSense.App.Components.Pages;
+
+public partial class Home
+{
+    // Remove and register as a service
+    private static MLContext MlContext { get; set; } = new MLContext(); 
+    private SentimentService SentimentService { get; set; } = new SentimentService(MlContext);
+    private string SentimentText { get; set; }
+
+    private void AnalyzeSentiment()
+    {
+        var trainingData = SentimentService.LoadData().TrainSet;
+        var model =  SentimentService.BuildAndTrainModel(trainingData);
+        SentimentService.UseModelWithSingleEntity(model, SentimentText);
+    }
+}
