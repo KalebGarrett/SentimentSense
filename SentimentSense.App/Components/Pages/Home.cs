e567d6a340ff7c220c8937ea405319c061c2eb45
Kaleb Garrett <kalebagarrett@gmail.com>
2024-07-31 18:18:39 -0400

    Created cards for text box and metrics

--- SentimentSense.App/Components/Pages/Home.cs
+++ SentimentSense.App/Components/Pages/Home.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
 using SentimentSense.App.Services;
 
 namespace SentimentSense.App.Components.Pages;
@@ -8,41 +9,66 @@ public partial class Home
     [Inject] private SentimentService SentimentService { get; set; }
     private string SentimentText { get; set; }
     private string Sentiment { get; set; }
-    private string Probability { get; set; }
-    private static string Accuracy { get; set; }
-    private static string AreaUnderRocCurve { get; set; }
-    private static string F1Score { get; set; }
+    private double Probability { get; set; }
+    private static double Accuracy { get; set; }
+    private static double AreaUnderRocCurve { get; set; }
+    private static double F1Score { get; set; }
     private int Index = -1;
     private int DataSize { get; set; } = 3;
-    private double[] Data { get; set; } =
-        {Convert.ToDouble(Accuracy), Convert.ToDouble(AreaUnderRocCurve), Convert.ToDouble(F1Score)};
-
+    private double[] Data { get; set; } 
     private string[] Labels { get; set; } = {"Accuracy", "AreaUnderRocCurve", "F1Score"};
+    private Position LegendPosition { get; set; } = Position.Bottom;
+    private Color FullColor { get; set; }
 
-    private void AnalyzeSentiment()
+    protected override async Task OnInitializedAsync()
+    {
+        Accuracy = 0;
+        AreaUnderRocCurve = 0;
+        F1Score = 0;
+    }
+    
+    private void AnalyzeText()
     {
         var trainingData = SentimentService.LoadData().TrainSet;
         var model = SentimentService.BuildAndTrainModel(trainingData);
 
         var resultPrediction = SentimentService.UseModelWithSingleEntity(model, SentimentText);
         Sentiment = resultPrediction.Prediction ? "Positive" : "Negative";
-        Probability = $"{resultPrediction.Probability:P2}";
+        Probability = resultPrediction.Probability * 100;
+  
+        FullColor = resultPrediction.Prediction ? Color.Tertiary : Color.Secondary;
 
         var testData = SentimentService.LoadData().TestSet;
         var metrics = SentimentService.EvaluateModel(model, testData);
-        Accuracy = $"Accuracy: {metrics.Accuracy:P2}";
-        AreaUnderRocCurve = $"AreaUnderRocCurve: {metrics.AreaUnderRocCurve:P2}";
-        F1Score = $"F1Score: {metrics.F1Score:P2}";
-        Console.WriteLine(Accuracy);
+        Accuracy = metrics.Accuracy * 100;
+        AreaUnderRocCurve = metrics.AreaUnderRocCurve * 100;
+        F1Score = metrics.F1Score * 100;
+
+        Data = new[] {Convert.ToDouble(Accuracy), Convert.ToDouble(AreaUnderRocCurve), Convert.ToDouble(F1Score)};
     }
 
-    private void RandomizeData()
+    private void OnSelectedValue(Position value)
     {
-        var random = new Random();
-        var new_data = new double[DataSize];
-        for (int i = 0; i < new_data.Length; i++)
-            new_data[i] = random.NextDouble() * 100;
-        Data = new_data;
-        StateHasChanged();
+        switch(value)
+        {
+            case Position.Top:
+                LegendPosition = Position.Top;
+                break;
+            case Position.Left:
+                LegendPosition = Position.Left;
+                break;
+            case Position.Right:
+                LegendPosition = Position.Right;
+                break;
+            case Position.Bottom:
+                LegendPosition = Position.Bottom;
+                break;
+            case Position.Start:
+                LegendPosition = Position.Start;
+                break;
+            case Position.End:
+                LegendPosition = Position.End;
+                break;
+        }
     }
 }
