4fbbcbbf381c47657426ef829534a345a2ec7343
Kaleb Garrett <kalebagarrett@gmail.com>
2024-08-13 16:24:54 -0400

    Added interface for mongo repo

---
+++ SentimentSense.API/Repositories/Interfaces/IMongoRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using SentimentSense.Models;
+
+namespace SentimentSense.API.Repositories.Interfaces;
+
+public interface IMongoRepository
+{
+    public Task<IEnumerable<BaseModel>> FindAll();
+    public Task<BaseModel> FindById(string id);
+    public Task<BaseModel> InsertOne(BaseModel data);
+    public Task<BaseModel> InsertMany(ICollection<BaseModel> documents);
+    public Task<BaseModel> ReplaceOne(string id, BaseModel data);
+    public Task<BaseModel> DeleteById(string id, bool hardDelete = false);
+    public Task<BaseModel> DeleteMany(Expression<Func<BaseModel, bool>> filterExpression);
+}
